657134d65a36a53a66ec74fec97848be77006cba
wsyu <wsyu0921@163.com>
2023-01-04 11:49:56 +0800

    修复推送逻辑

--- tasks.py
+++ tasks.py
@@ -47,6 +47,7 @@ async def stock_save_db(stock_list: List):
     如果未存在，直接添加
     """
     day = local_day_str()
+    push_stock_str = ''
     for stock in stock_list:
         stock_code = stock[0:6]
         stock_name = stock[6:]
@@ -80,8 +81,12 @@ async def stock_save_db(stock_list: List):
                                    turnover_rate=stock_info['turnover_rate'],
                                    circulation_market_value=stock_info['circulation_market_value'],
                                    )
+                push_stock_str += f"<p>{stock.code} {stock.name} {stock.last_price}</p>"
+
         except:
             continue
+    if push_stock_str:
+        await send_wechat(push_stock_str)
 
 async def search_gp(search_str: str):
     """
@@ -161,8 +166,8 @@ async def gp_start():
     print("股票任务开始...")
     # search_str = "上穿5日均线；上穿10日均线；股价大于20日均线；涨跌幅小于4；日rsi金叉；市盈ttm大于0；量比大于2.7；外盘/内盘≥1.3；股价在3到35元之间；非中字头；非科创板；非*st；非北交所；非银行股；"
     # search_str = '上穿5日均线；上穿10日均线；股价大于20日均线；涨跌幅小于4；市盈ttm大于0；量比大于2；外盘/内盘≥1.3；股价在3到35元之间；非中字头；非科创板；非*st；非北交所；非银行股；'
-    sw_search_str = '上穿5日均线；上穿10日均线；股价大于20日均线；最大涨幅<4%；市盈ttm大于0；股价在3到35元之间；非中字头；非科创板；非*st；非北交所；非银行股；资金流入的；换手率大于上一交易日换手率的0.8倍以上；'
-    xw_search_str = '上穿5日均线；上穿10日均线；股价大于20日均线；最大涨幅<4%；；市盈ttm大于0；股价在3到35元之间；非中字头；非科创板；非*st；非北交所；非银行股；资金流入的；换手率大于上一交易日换手率的1.2倍以上；'
+    sw_search_str = '上穿5日均线；上穿10日均线；股价大于20日均线；最大涨幅<4%；kdj金叉；市盈ttm大于0；股价在3到35元之间；非中字头；非科创板；非*st；非北交所；非银行股；资金流入的；换手率大于上一交易日换手率的0.8倍以上；'
+    xw_search_str = '上穿5日均线；上穿10日均线；股价大于20日均线；最大涨幅<4%；kdj金叉；市盈ttm大于0；股价在3到35元之间；非中字头；非科创板；非*st；非北交所；非银行股；资金流入的；换手率大于上一交易日换手率的1.2倍以上；'
     # scheduler.add_job(search_gp, 'interval', seconds=20, id='test', args=[search_str],
     #                   replace_existing=True, timezone='Asia/Shanghai')
     if is_trading_day():
@@ -175,10 +180,11 @@ async def gp_start():
                           start_date=dt_9_38, end_date=dt_11_30, replace_existing=True, timezone='Asia/Shanghai')
         scheduler.add_job(search_gp, 'interval', seconds=150, id='xw', args=[xw_search_str],
                           start_date=dt_13_00, end_date=dt_14_48, replace_existing=True, timezone='Asia/Shanghai')
-        scheduler.add_job(push_wechat, 'interval', seconds=120, id='auto_push_wechat', start_date=dt_9_38,
-                          end_date=dt_14_48, replace_existing=True, timezone='Asia/Shanghai')
+        # scheduler.add_job(push_wechat, 'interval', seconds=120, id='auto_push_wechat', start_date=dt_9_38,
+        #                   end_date=dt_14_48, replace_existing=True, timezone='Asia/Shanghai')
     else:
-        print("今天不是交易日~~~")
+        await send_wechat("今天不是交易日~~~")
+        # print("今天不是交易日~~~")
 
 async def keep_db_conn():
     print(f"当前时间：{datetime.now().strftime( '%Y-%m-%d %H:%M:%S')}, 服务正常...")
@@ -187,7 +193,7 @@ async def add_task():
     print("正在启动定时任务...")
     scheduler.add_job(keep_db_conn, 'interval', id='keep_db', seconds=60*60, timezone='Asia/Shanghai',
                       replace_existing=True)
-    scheduler.add_job(gp_start, 'cron', id='push_start', hour='9', minute='00', timezone='Asia/Shanghai',
+    scheduler.add_job(gp_start, 'cron', id='push_start', hour='09', minute='00', timezone='Asia/Shanghai',
                       replace_existing=True)
     # scheduler.add_job(gp_start, 'interval', id='push_start', seconds=30, timezone='Asia/Shanghai',
     #                   replace_existing=True)
